bd1031a5af9accafa177cf4528a0961af24fd1fe
toriqultonu <toriqultonu22@gmail.com>
2021-04-10 00:01:24 -0700

    Firstday

---
+++ Python/Demon/main.py
@@ -0,0 +1,90 @@
+from turtle import Turtle, Screen
+
+turtle = Turtle()
+
+
+turtle.shape("turtle")
+turtle.color("red", "green")
+
+def turtle_go():
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(45)
+    turtle.forward(25)
+    turtle.left(45)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.left(135)
+    turtle.forward(25)
+    turtle.right(45)
+    turtle.forward(50)
+    turtle.right(135)
+    turtle.forward(25)
+    turtle.left(45)
+    turtle.forward(50)
+    turtle.left(135)
+    turtle.forward(25)
+    turtle.left(135)
+    turtle.forward(50)
+    turtle.left(45)
+    turtle.forward(25)
+
+turtle_go()
+turtle_go()
+turtle_go()
+turtle_go()
+turtle_go()
+turtle_go()
+turtle_go()
+turtle_go()
+turtle.left(90)
+turtle.forward(100)
+turtle_go()
+turtle_go()
+turtle_go()
+turtle_go()
+turtle_go()
+turtle_go()
+turtle_go()
+turtle_go()
+
+turtle.left(90)
+turtle.forward(100)
+turtle_go()
+turtle_go()
+turtle_go()
+turtle_go()
+turtle_go()
+turtle_go()
+turtle_go()
+turtle_go()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
