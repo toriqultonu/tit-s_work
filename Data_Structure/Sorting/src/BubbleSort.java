41cbc3c8535d8713e61914807a17e3765e1202fa
toriqultonu <toriqultonu22@gmail.com>
2021-04-15 14:39:43 -0700

    Complete bubble sort
    Here I've learnt the differences between c/c++ pointer and java reference quite well.

--- Data_Structure/Sorting/src/BubbleSort.java
+++ Data_Structure/Sorting/src/BubbleSort.java
@@ -1,5 +1,32 @@
 public class BubbleSort {
 
-    void sort()
+    void Bubble(int a[]){
+        int i, j;
+        for(i=0;i<a.length-1;i++){
+            for(j=0;j<a.length-i-1;j++){
+                if(a[j]>a[j+1]){
+                    int temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                }
+
+            }
+        }
+    }
+
+     void display(int[] a){
+       for(int i=0;i<a.length;i++){
+           System.out.println(a[i]);
+       }
+    }
+
+    public static void main(String[] args){
+        int A[] = {2,3,6,2,7,97,5,3,5};
+
+        BubbleSort ob = new BubbleSort();
+        ob.Bubble(A);
+        ob.display(A);
+
+    }
 
 }
