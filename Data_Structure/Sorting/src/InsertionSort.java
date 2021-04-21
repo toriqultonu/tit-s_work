0f02fa10bac219f6d669b6520d828c9e2d5cfc33
toriqultonu <toriqultonu22@gmail.com>
2021-04-21 05:02:54 -0700

    InsertionSort
    Insertion sort in java coded

---
+++ Data_Structure/Sorting/src/InsertionSort.java
@@ -0,0 +1,30 @@
+public class InsertionSort {
+
+    void sort(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+    static void display(int arr[])
+    {
+        for(int a:arr){
+            System.out.print(a+", ");
+        }
+    }
+
+    public static void main(String[] args){
+        int arr[] = {13, 4, 64, 6, 23, 6, 5};
+
+        InsertionSort ob = new InsertionSort();
+        ob.sort(arr);
+        ob.display(arr);
+    }
+}
